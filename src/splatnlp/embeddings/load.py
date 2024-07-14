dca7f20f3670cb83e2e8c5521fa652bc35aa1c6d
Cesar E Garza <cesar@cegarza.com>
2024-07-13 22:51:09 -0500

    rename to more appropriate module name

--- src/splatnlp/embeddings/load.py
+++ src/splatnlp/embeddings/load.py
@@ -1,6 +1,7 @@
 import logging
 
 import pandas as pd
+from gensim.models import Doc2Vec
 
 logger = logging.getLogger(__name__)
 
@@ -19,3 +20,17 @@ def load_data(path: str | None = None) -> pd.DataFrame:
     return pd.read_csv(
         path, dtype={"weapon_id": "Int64", "ability_tags": "str"}
     )
+
+
+def load_model(path: str | None = None) -> Doc2Vec:
+    """Load a Doc2Vec model from a file.
+
+    Args:
+        path (str): The path to the model file.
+
+    Returns:
+        Doc2Vec: The loaded Doc2Vec model.
+    """
+    path = path or "models/doc2vec.model"
+    logger.info("Loading model from %s", path)
+    return Doc2Vec.load(path)
